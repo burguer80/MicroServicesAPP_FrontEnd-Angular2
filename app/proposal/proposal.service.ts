a0cbf9c10f106d226ce4320f7414bff1b8ef92bd
Sergio Burgueño <burguer@Sergios-MacBook-Pro.local>
2016-12-11 19:26:55 -0800

    Show Page Component API

--- app/proposal/proposal.service.ts
+++ app/proposal/proposal.service.ts
@@ -6,7 +6,7 @@ import { Proposal } from  './proposal';
 
 @Injectable()
 export class ProposalService{
-  private proposalsUrl = 'http://localhost:3002/proposals.json';
+  private proposalsUrl = 'http://localhost:3002/proposals';
 
   constructor(
     private http: Http
@@ -16,6 +16,9 @@ export class ProposalService{
     return this.http.get(this.proposalsUrl)
                     .map((response: Response) => <Proposal[]>response.json())
                     .catch(this.handleError);
+  }
+  getProposal(id: number) {
+      return this.http.get(this.proposalsUrl + "/" + id + '.json');
   }
 
   private handleError (error: Response | any) {
